dcb404ef6315f6c8cd2f93bc39aa7ad3df94f08d
Geinel Niño Dungao <geineldungao012@gmail.com>
2026-05-07 23:01:35 +0800

    add assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  assetPrefix: "https://storage.googleapis.com/YOUR_BUCKET_NAME",
   images: {
     unoptimized: true,
   },
