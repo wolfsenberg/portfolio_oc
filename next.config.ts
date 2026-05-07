5fa521102f84717a9f5a51ac510cef6f02f072e4
Geinel Niño Dungao <geineldungao012@gmail.com>
2026-05-07 23:08:24 +0800

    update asset prefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: "https://storage.googleapis.com/YOUR_BUCKET_NAME",
+  assetPrefix: "https://storage.googleapis.com/geinel-portfolio",
   images: {
     unoptimized: true,
   },
